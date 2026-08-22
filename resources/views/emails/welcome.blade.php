c063155b09a4902085e917053b24ea7b73cbaad7
fearlessalan <fearlessalain@gmail.com>
2026-08-22 19:02:38 +0200

    feat: implement welcome email service and integrate into user registr… (#19)
    * feat: implement welcome email service and integrate into user registration flow with associated tests
    
    * chore: update league/commonmark to v2.10.0 and add type hinting to WelcomeUserMail attachments

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,12 @@
+<x-mail::message>
+# Bienvenue, {{ $name }} !
+
+Merci de vous être inscrit sur la plateforme CDTM. Nous sommes ravis de vous compter parmi nous.
+
+<x-mail::button :url="config('app.url')">
+Accéder à mon espace
+</x-mail::button>
+
+Cordialement,<br>
+L'équipe {{ config('app.name') }}
+</x-mail::message>
