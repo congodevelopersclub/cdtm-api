912f443d9165dcaac8dbd3e088b61ad820d0895d
berna39 <kalemajoseph39@gmail.com>
2026-08-23 18:23:44 +0200

    fix: :hammer: update redirect link in the welcoming mail

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -3,7 +3,7 @@
 
 Merci de vous être inscrit sur la plateforme CDTM. Nous sommes ravis de vous compter parmi nous.
 
-<x-mail::button :url="config('app.url')">
+<x-mail::button :url="config('services.frontend_url')">
 Accéder à mon espace
 </x-mail::button>
 
